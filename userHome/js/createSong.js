ccec0ed01fef77847b72501eb91ff32eb7a1f494
TuanAnhTran29 <tuananhtranquoc@gmail.com>
2022-08-16 22:31:34 +0700

    Added folder CaseStudy4_FE

--- userHome/js/createSong.js
+++ userHome/js/createSong.js
@@ -2,11 +2,17 @@ var image = '';
 // firebase bucket name
 // REPLACE WITH THE ONE YOU CREATE
 // ALSO CHECK STORAGE RULES IN FIREBASE CONSOLE
-var fbBucketName = 'song';
+var fbBucketName1 = 'song';
+var fbBucketName2 = 'picture';
 // get elements
 var uploader = document.getElementById('uploader');
 var fileButton = document.getElementById('fileButton');
-let downloadURL= ""
+
+var uploaderPicture= document.getElementById("uploaderPicture");
+var pictureButton= document.getElementById("pictureButton");
+
+let downloadURL1= ""
+let downloadURL2= ""
 // listen for file selection
 fileButton.addEventListener('change', function (e) {
 
@@ -14,19 +20,19 @@ fileButton.addEventListener('change', function (e) {
     console.log('file upload event', e);
 
     // get file
-    var file = e.target.files[0];
+    var file1 = e.target.files[0];
 
     // create a storage ref
-    var storageRef = firebase.storage().ref(`${fbBucketName}/${file.name}`);
+    var storageRef1 = firebase.storage().ref(`${fbBucketName1}/${file1.name}`);
 
     // upload file
-    var uploadTask = storageRef.put(file);
+    var uploadTask1 = storageRef1.put(file1);
 
     // The part below is largely copy-pasted from the 'Full Example' section from
     // https://firebase.google.com/docs/storage/web/upload-files
 
     // update progress bar
-    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+    uploadTask1.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
         function (snapshot) {
             // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
             var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
@@ -60,7 +66,64 @@ fileButton.addEventListener('change', function (e) {
         }, function () {
             // Upload completed successfully, now we can get the download URL
             // save this link somewhere, e.g. put it in an input field
-             downloadURL = uploadTask.snapshot.downloadURL;
+             downloadURL1 = uploadTask1.snapshot.downloadURL;
+
+        })
+})
+
+pictureButton.addEventListener('change', function (e) {
+
+    // what happened
+    console.log('file upload event', e);
+
+    // get file
+    var file2 = e.target.files[0];
+
+    // create a storage ref
+    var storageRef2 = firebase.storage().ref(`${fbBucketName2}/${file2.name}`);
+
+    // upload file
+    var uploadTask2 = storageRef2.put(file2);
+
+    // The part below is largely copy-pasted from the 'Full Example' section from
+    // https://firebase.google.com/docs/storage/web/upload-files
+
+    // update progress bar
+    uploadTask2.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+        function (snapshot) {
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            uploaderPicture.value = progress;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED: // or 'paused'
+                    console.log('Upload is paused');
+                    break;
+                case firebase.storage.TaskState.RUNNING: // or 'running'
+                    console.log('Upload is running');
+                    break;
+            }
+        }, function (error) {
+
+            // A full list of error codes is available at
+            // https://firebase.google.com/docs/storage/web/handle-errors
+            switch (error.code) {
+                case 'storage/unauthorized':
+                    // User doesn't have permission to access the object
+                    break;
+
+                case 'storage/canceled':
+                    // User canceled the upload
+                    break;
+
+                case 'storage/unknown':
+                    // Unknown error occurred, inspect error.serverResponse
+                    break;
+            }
+        }, function () {
+            // Upload completed successfully, now we can get the download URL
+            // save this link somewhere, e.g. put it in an input field
+            downloadURL2 = uploadTask2.snapshot.downloadURL;
 
         })
 })
@@ -72,8 +135,8 @@ function createSong(){
                     let artist= $("#InputArtist").val()
                     let lyrics= $("#InputLyrics").val()
                     let category= $("input[type= radio]:checked").val()
-                    let image= $("#image").val()
-                    let path= downloadURL
+                    let image= downloadURL2
+                    let path= downloadURL1
                     let newSong= {
                         name: songName,
                         artist: artist,
