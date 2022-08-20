7430f3c248b3d9f332319610720b91a20145b3f4
TuanAnhTran29 <tuananhtranquoc@gmail.com>
2022-08-20 17:31:29 +0700

    Added folder CaseStudy4_FE

--- userHome/js/userHome.js
+++ userHome/js/userHome.js
@@ -17,9 +17,16 @@ function showListSong(){
                     '                <div class="fw-bold">'+ data[i].name +'</div>\n' + data[i].artist +
                     '                \n' +
                     '            </div>\n' +
-                    '        <audio style="width: 80%" controls><source src="'+ data[i].path +' "></audio>\n' +
-                    '            <button onclick="doLike_Dislike(' + data[i].id + ')" style="font-size:24px;background-color: white "><i class="fa fa-heart-o"></i></button>\n' +
-                    '        </li>'
+                    '            <button onclick="backwardSong('+ i +')">\n' +
+                    '                <img src="https://snowleo208.github.io/100-Days-of-Code/7.%20Music%20Player/img/backward.svg" alt="Backward">\n' +
+                    '            </button>'+
+                    '            <button onclick="forwardSong('+ i +')">\n' +
+                    '                <img src="https://snowleo208.github.io/100-Days-of-Code/7.%20Music%20Player/img/forward.svg" alt="Forward">\n' +
+                    '            </button>'+
+                    '            <audio style="width: 80%" controls><source src="'+ data[i].path +' "></audio>\n' +
+                    '                <button onclick="doLike_Dislike(' + data[i].id + ')" style="font-size:24px;background-color: white "><i class="fa fa-heart-o"></i></button>\n' +
+
+                    '       </li>'
             }
             document.getElementById("listSong").innerHTML= listSong
         }
@@ -46,3 +53,20 @@ function doLike_Dislike(id){
 }
 
 showListSong()
+
+
+function backwardSong(index){
+    let currentUser= JSON.parse(localStorage.getItem("user"))
+    $.ajax({
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + currentUser.token
+        },
+        type: "GET",
+        url: "https://casestudy4.herokuapp.com/api/song/backward/" + index,
+        success: function (data){
+            data.play();
+        }
+    })
+}
